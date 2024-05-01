64986d0e2470aba255d8513f96ab45fbab9258a1
hashiwa000 <gate000jp@gmail.com>
2024-05-01 10:12:44 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 HOME_DIR=$(cd $(dirname $0);pwd)
-java -classpath ${HOME_DIR}/classes jp.hashiwa.reversi.play.PlayerVSComputer $1 $2
+
+PROPS=""
+#PROPS="\
+#  -Djp.hashiwa.reversi.alphabeta.depth=5 \
+#  -Djp.hashiwa.reversi.minmax.depth=3 \
+#"
+
+java \
+  -classpath ${HOME_DIR}/classes \
+  -server \
+  $PROPS \
+  jp.hashiwa.reversi.play.PlayerVSComputer $1 $2
